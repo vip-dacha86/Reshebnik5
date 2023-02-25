2deab17938b23a11a26b0e30115c24a17679d6a8
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-25 20:05:28 +0500

    task17 искать число в массиве

---
+++ task17_massiv/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив .Напишите программу которая определяет присутствует ли заданное число в массиве.
+// 4->[6,7,19,345,3]->no
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (FindElement(baseArray, n))
+{
+   Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+int[] GetArrayFromString (string stringArray)
+{
+    string[] nums =stringArray.Split(" ");
+    int[] res = new int[nums.Length];
+    for (int i =0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums [i]);
+    } 
+    return res;
+}
+bool FindElement(int[] array, int el)
+{
+    foreach ( var item in array)
+    {
+        if (el == item) return true;
+    }
+    return false;
+}

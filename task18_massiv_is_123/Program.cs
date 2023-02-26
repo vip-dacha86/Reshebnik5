50a60998b28e2f2b7cbe646093b6004611fdefd9
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-26 09:54:06 +0500

    task18_massiv_is_123

---
+++ task18_massiv_is_123/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив из 123 случайных чисел[0:150].
+// Найдите количество элементов массива значения которых лежат в отрезке [10,99]
+Console.Clear();
+int []startArray = GetArray(123,0,150);
+Console.WriteLine($"Количество элементов в отрезке[10,99]={GetCountElements(startArray,10,99)}");
+int[]GetArray(int size,int minValue,int maxValue)
+{
+    int[]res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue +1);
+    }
+    return res;
+}  
+int GetCountElements(int[]array,int leftRange, int rightRange)
+{
+    int count =0;
+    foreach (int item in array)
+    {
+        if (item >= leftRange && item <=rightRange)count++;
+    }
+    return count;
+}
